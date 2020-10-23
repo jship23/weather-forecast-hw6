59f03f9949c5453e6a47fec10eff47e61d328c80
Jordan Shipley <jordan.j.shipley@gmail.com>
2020-10-22 19:03:04 -0600

    changes

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
+// define api key variable
+var apiKey = "0ffcc1aa1c7ff0e151c44cb2f4864c7a";
 
-var APIkey = "0ffcc1aa1c7ff0e151c44cb2f4864c7a";
-var queryURL5day = "api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIkey;
+//Search button click event
+$("#searchBtn").on("click", function (event) {
+  event.preventDefault();
+  var cityName = $("#city-name").val();
+  var queryURLCurrent =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityName +
+    "&appid=" +
+    apiKey;
+  var queryURL5day =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    cityName +
+    "&appid=" +
+    apiKey;
+
+//API call for current weather
+  $.ajax({
+    url: queryURLCurrent,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    var cityName = response.cityName;
+    var temp = response.main.temp;
+    var humidity = response.main.humidity;
+    var windSpeed = response.wind.speed;
+
+    
+
+  });
 
 
-$("#searchBtn").on("click", function(event){
-    event.preventDefault();
-    var cityName = $("#city-name").val();
-    var queryURLCurrent = "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey;
-
-    $.ajax({
-        url: queryURLCurrent,
-        method: "GET"
-    }).then(function(response) {
-        
-      });
-      })
-
-})
+//API call for 5 day forecast
+  $.ajax({
+    url: queryURL5day,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+  });
+});
